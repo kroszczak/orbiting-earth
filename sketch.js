782b50c5862e210056086199d6fb17d47ccd6d6a
Kacper Roszczak <k.roszczak@interia.pl>
2020-01-29 21:34:15 +0100

    added earth self rotation

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ const stars = [];
 let earth = new object(0, 0, 80, 0, 400, null);
 let moon = new object(0, 0, 20, 0, 100, null);
 
-
+let x8 = 0;
 
 function preload(){
   earth.img = loadImage('images/earth.png');
@@ -29,6 +29,7 @@ function preload(){
 function setup() {
   createCanvas(windowWidth, windowHeight);
   imageMode(CENTER);
+  angleMode(DEGREES); 
 
   for(let i = 0; i < 80; i++){
     stars.push(new object(random(0, 2100), random(0, 1200), random(1, 9), null));
@@ -41,12 +42,18 @@ function draw() {
   stat();
 
   earth.x = width/2 + earth.orbit * cos(earth.angle); earth.y =  height/2 + earth.orbit * sin(earth.angle);
-  image(earth.img, earth.x, earth.y, earth.rad, earth.rad);//earth
-  earth.angle += 0.02;
+
+  push();
+    translate(earth.x, earth.y);
+    rotate(x8);
+    x8 += 0.8;
+    image(earth.img, 0, 0, earth.rad, earth.rad);//earth
+    earth.angle += 0.664;
+  pop();
 
   fill(200);
   circle(earth.x + moon.orbit * cos(moon.angle), earth.y + moon.orbit * sin(moon.angle), moon.rad);
-  moon.angle += 0.15
+  moon.angle += -2;
  
   
 }
