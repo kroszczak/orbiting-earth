647789d3fdbca37b60110f783a4368ca4e669241
Kacper Roszczak <k.roszczak@interia.pl>
2020-01-30 15:14:54 +0100

    asteroid's track changed

--- sketch.js
+++ sketch.js
@@ -16,26 +16,29 @@ class test_class{
     this.history.unshift(createVector(this.x, this.y));
     arr = this.history.slice(0, 100); 
     arr.forEach((item, index) => {
-      index < 15 ? square(item.x, item.y, 3): index < 30 ? square(item.x, item.y, 2.6): index < 45 ? square(item.x, item.y, 2.4): index < 60 ? square(item.x, item.y, 2.2): index < 75 ? square(item.x, item.y, 1.9): index < 85 ? square(item.x, item.y, 1.6): index < 95? square(item.x, item.y, 1.2): square(item.x, item.y, 0.8);
+      index < 15 ? square(item.x + random(-2, +2), item.y + random(-2, +2), 6): index < 30 ? square(item.x + random(-2, +2), item.y + random(-2, +2), 5): index < 45 ? square(item.x + random(-2, +2), item.y + random(-2, +2), 4): index < 60 ? square(item.x + random(-2, +2), item.y + random(-2, +2), 3): index < 75 ? square(item.x + random(-2, +2), item.y + random(-2, +2), 1.9): index < 85 ? square(item.x + random(-2, +2), item.y + random(-2, +2), 1.6): index < 95? square(item.x + random(-2, +2), item.y + random(-2, +2), 1.2): square(item.x + random(-2, +2), item.y + random(-2, +2), 1);
     })
   }
 }
+
 
 class star{
   constructor(x, y, r){
     this.x = x;
     this.y = y;
-    this.rad = r;
+    this.r = r;
   }
 }
+
+
 class object{
 
-  constructor(rad, orbit, options, move, rot_chng, satellites){
+  constructor(r, orbit, options, move, rot_chng, satellites){
 
     this.x = 0;
     this.y = 0; 
     this.history = [];
-    this.rad = rad;
+    this.r = r;
     this.angle = 0;
     this.orbit = orbit;
     this.img = options.img;
@@ -46,10 +49,10 @@ class object{
     this.satellites = satellites || [];
   }
 
-  display(xp, yp){
+  display(x, y){
     
-    this.x = xp + this.orbit * cos(this.angle);
-    this.y = yp + this.orbit * sin(this.angle);
+    this.x = x + this.orbit * cos(this.angle);
+    this.y = y + this.orbit * sin(this.angle);
     this.history.unshift(createVector(this.x, this.y));
     arr = this.history.slice(0, 100); 
     arr.forEach((item, index) => {
@@ -57,11 +60,12 @@ class object{
     })
     fill(255);
     push();
-    translate(this.x, this.y);
-      rotate(this.rot); this.rot += this.rot_change;
+      translate(this.x, this.y);
+      rotate(this.rot); this.rot += this.rot_change; this.angle += this.move;
+
       if (this.col) fill(this.col[0], this.col[1], this.col[2]);
-      this.img == null ? circle(0, 0, this.rad) :image(this.img, 0, 0, this.rad, this.rad);
-      this.angle += this.move;
+      this.img == null ? circle(0, 0, this.r) :image(this.img, 0, 0, this.r, this.r);
+      
     pop();
 
     for(let i = 0; i < this.satellites.length; i++){
@@ -70,18 +74,26 @@ class object{
 }
 
 
+
+
+
+let arr = [];
 const stars = [];
 const ast = [];
 const objects = [
   moon = new object(8, 50, {col: 200}, 9, 0.6),
   wenus = new object(22, 200, {col: [40, 90, 240]}, 0.8, 0),
   merkury = new object(13, 115, {col: [230, 230, 230]}, 1.2, 2),
-  earth = new object(40, 300, {img: "images/earth.png"}, 0.5, -1.5, [moon]),
+  earth = new object(40, 300, {img: "images/earth.png"}, 0.5, 1.5, [moon]),
   mars = new object(30, 420, {col: [243, 130, 53]}, 0.3, 0),
   sun = new object(100, 0, {col: [243, 190, 23]}, 0, 0, [earth, merkury, wenus, mars]),
   solar_system = new object(0, 0, {col: 0}, 0, 0, [sun])
 ];
-let arr = [];
+
+
+
+
+
 
 
 function preload(){
@@ -95,33 +107,24 @@ function setup() {
   imageMode(CENTER);
   angleMode(DEGREES); 
   noStroke();
-
   for(let i = 0; i < 100; i++){
-    stars.push(new star(random(0, 2100), random(0, 1200), random(1, 9)));
-  }
-  let rand = random(6, 60);
+    stars.push(new star(random(0, 2100), random(0, 1200), random(1, 9)));}
+  let rand = random(6, 7);
   for(let i = 0; i < rand; i++){
-    ast.push(new test_class(random(2,4), random(1,3), random(-1000, -100), random(-1000, 1800), random(7, 22)));
-  }
+    ast.push(new test_class(random(2,4), random(1,3), random(0, 0), random(0, 1800), random(20, 22)));}}
 
-}
 
 
 function draw() {
-
   stat();
   for(let i = 0; i < ast.length; i++){
-    ast[i].display();
-  }
-  solar_system.display(width/2, height/2);
+    ast[i].display();}
+  solar_system.display(width/2, height/2);}
 
-}
+
 
 function stat(){
   background(10, 15, 27);
-  
   fill(255);
   for(let i = 0; i < 100; i++){
-    circle(stars[i].x, stars[i].y, stars[i].rad);
-  }
-}
+    circle(stars[i].x, stars[i].y, stars[i].r);}}
